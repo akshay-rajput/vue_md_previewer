29ce971f393e422487a5676d190be1d7fe1a03a9
Akshay <akshayrajput102@gmail.com>
2020-12-07 22:05:58 +0530

    Base version of app.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <i-layout>
+      <i-layout-header class="_padding-0">
+        <i-navbar :collapse = false>
+          <i-navbar-brand>
+            <span class="">
+              <svg xmlns="http://www.w3.org/2000/svg" class="theme_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
+            </span> 
+            <span class="logo">ZAP</span>
+          </i-navbar-brand>
+          <i-nav>
+              <i-nav-item>
+                <span class="">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="theme_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                  </svg>
+                </span>
+
+                <span class="_text-black">
+                  <!-- <svg xmlns="http://www.w3.org/2000/svg" class="theme_icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                  </svg> -->
+                  <svg xmlns="http://www.w3.org/2000/svg" class="theme_icon" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                  </svg>
+                </span>
+              </i-nav-item>
+          </i-nav>
+        </i-navbar>
+      </i-layout-header>
+      
+      <i-layout-content>
+          <i-container>
+            <h3 class="_margin-y-1">Upload a file</h3>
+            <p class="_margin-y-1">OR</p>
+            <h3 class="_margin-y-1">Use online editor</h3>
+
+            <i-row class="._display-flex _align-items-stretch _flex-direction-sm-row">
+              <i-column sm="6">
+                <label for="md_text">Your Text</label>
+                <i-textarea id="md_text" class="_width-100" v-model="mdtext" placeholder="Enter your Markdown text here." rows="15"></i-textarea>
+              </i-column>
+              
+              <i-column sm="6" class="_display-flex _flex-direction-column">
+                <label for="">Markdown Preview</label>
+                <vue-markdown :source="mdtext"  class="preview-box _flex-grow-1"></vue-markdown>
+              </i-column>
+            </i-row>
+
+          </i-container>          
+      </i-layout-content>
+      
+    </i-layout>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      mdtext: ''
+    }
+  },
   components: {
-    HelloWorld
+    VueMarkdown
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Poppins&family=Ranchers&display=swap');/* font-family: 'Bowlby One SC', cursive;
+font-family: 'JetBrains Mono', monospace;
+font-family: 'Poppins', sans-serif; */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Poppins', sans-serif;
+  height: 100%;
+  background: rgb(98, 93, 133);
+  /* text-align: center; */
+  color: #333;
+  /* margin-top: 60px; */
+}
+
+.logo{
+  font-family: 'Ranchers', cursive;
+  font-size: 1.75rem;
+  line-height: 1
+  }
+
+h1, h2, h3, h4, h5{
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+}
+
+#md_text{
+  font-family: 'JetBrains Mono', monospace;
+}
+
+.theme_icon{
+  color: black;
+  height: 28px;
+  width: 28px;
+}
+.preview-box{
+  border: 1px solid #ccc;
+  padding-left: 10px;
+  /* height: 100%; */
+  width: 100%;
+  /* height: 15rem; */
 }
 </style>
